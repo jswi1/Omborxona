48cc2344c488fea063080d7e7050729a85ef4f75
jswi1 <zwxsmpipa@gmail.com>
2022-06-12 23:38:41 +0500

    Dryarli tugadi

--- asosiy/views.py
+++ asosiy/views.py
@@ -40,11 +40,37 @@ class MahsulotEdit(View):
 
 class ClientView(View):
     def get(self, request):
-        return render(request, "clients.html")
+        if request.user.is_authenticated:
+            o = Ombor.objects.get(user=request.user)
+            m = Mahsulot.objects.filter(ombor=o)
+            return render(request, "client.html", {"clients":m})
+        return redirect("login")
+
+    def post(selfself, request):
+        o = Ombor.objects.get(user=request.user)
+        Mahsulot.objects.create(
+            ism = request.POST.get("i"),
+            dokon = request.POST.get("d"),
+            tel=request.POST.get("t"),
+            manzil = request.POST.get("m"),
+            qarz = request.POST.get("q"),
+            ombor = o
+        )
+        return redirect("clientlar")
+
 
 class ClientEdit(View):
-    def get(self, request):
-        return render(request, "client_update.html")
+    def get(self, request, az):
+        c = Client.objects.get(id=az)
+        return render(request, "client_update.html", {"client":c})
+
+    def post(self, request, az):
+        Mahsulot.objects.filter(id=az).update(
+            miqdor=request.POST.get("m"),
+            kelgan_naex=request.POST.get("kn"),
+            sotuv_narx=request.POST.get("sn"),
+        )
+        return redirect("clientlar")
 
 class BolimView(View):
     def get(self, request):
